9555e1f107c8c6d078e6ed20fd452eb226038a74
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-03 11:53:18 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html',
+    './src/app/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
